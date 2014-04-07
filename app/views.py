4d3fe2b38a447d143204d912dcd4a03e52d4ecb5
Jaclyn Burge <jacburge@gmail.com>
2014-04-06 22:31:56 -0700

    Added resume in professional.html

--- app/views.py
+++ app/views.py
@@ -5,10 +5,22 @@ from flask import render_template
 def index():
 	return render_template('index.html')
 
+@app.route('/professional/')
+def professional():
+	return render_template('professional.html')
+
+@app.route('/creative/')
+def creative():
+	return render_template('creative.html')
+
+@app.route('/fun/')
+def fun():
+	return render_template('fun.html')
+
 @app.route('/portfolio/')
 def portfolio():
 	return render_template('portfolio.html')
 
-@app.route('/index/')
-def portfolio():
-	return render_template('boostrap_index.html')
+@app.route('/boot_index/')
+def boot_index():
+	return render_template('bootstrap_index.html')
